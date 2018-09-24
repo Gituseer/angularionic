79d4976d6b28a56d484ef8deb5b532b7a6fe487a
David Gomez <prueba@example.com>
2018-09-24 16:12:49 -0500

    Pruebas con variable Js y estructuras basicas

--- Js/script/app.js
+++ Js/script/app.js
@@ -1,1 +1,58 @@
-console.log("Hola mundo");
+
+/*
+var numero1= 10.5;
+var numero2= 5;
+var resultado= numero1*numero2;
+
+console.log(resultado);
+
+var nombre = ' David Gomez ';
+var profesion = " Desarrollador fullstack ";
+
+console.log(nombre+profesion);
+
+var tiene_mascota= true; 
+
+console.log(tiene_mascota);
+
+var datos=[ "David",26,false,25,28, "Desarrollador"]
+
+console.lag(datos[2]);
+
+
+var edad = 41;
+
+if (edad>=18 && edad<=40) {
+
+	console.log("Eres un adulto joven");
+}else{
+	console.log("No eres un adulto joven");
+}
+
+var dias=['Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo']
+
+for(var i=0;i<7;i++){
+	console.log(dias[i]);
+}
+
+*/
+function suma(numero1,numero2) {
+     var resultado=numero1+numero2;
+     return resultado; 
+     
+}
+
+var res=suma(88,55);
+
+console.log(res);
+
+
+
+
+
+
+
+
+
+
+
